dd1f9629310c1d0bb0010e9092cd0bc3e088b708
ph1014 <1575542398@qq.com>
2017-11-16 15:56:56 +0800

    后台主页面测试

--- tt-manager/tt-manager-web/src/main/java/com/tuto/web/empty.java
+++ tt-manager/tt-manager-web/src/main/java/com/tuto/web/empty.java
@@ -1,6 +1,16 @@
 package com.tuto.web;
 
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Scope("prototype")
 public class empty {
 
+    @RequestMapping("/")
+    public String doqwe(){
 
+        return "index";
+    }
 }
