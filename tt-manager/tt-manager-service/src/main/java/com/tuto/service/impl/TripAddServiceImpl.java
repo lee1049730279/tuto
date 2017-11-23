b06a33bf4a4a45651efbf96d123d5f49a3f8b544
Administrator <1079334695@qq.com>
2017-11-23 23:17:49 +0800

    完成景点图片上传

--- tt-manager/tt-manager-service/src/main/java/com/tuto/service/impl/TripAddServiceImpl.java
+++ tt-manager/tt-manager-service/src/main/java/com/tuto/service/impl/TripAddServiceImpl.java
@@ -120,7 +120,7 @@ public class TripAddServiceImpl implements TripAddService{
                 ttTripDetail.setTripId(tripId);
                 count+= ttTripDetailDao.insert(ttTripDetail);
                 System.out.println(count);
-                //添加 tt_trip_picture
+                //添加 tt_trip_picture表
                 String placeIntroduce = ttTripDetail.getPlaceIntroduce();
                 String title = StringUtils.substringBefore(placeIntroduce, "\" title");
                 String s1 = StringUtils.substringAfter(title, "src=\"");
